e2277e863564402a268251df755fc2d1b24eac76
Pushpa Basnet <puspabasnet97@gmail.com>
2025-07-24 18:48:52 +0545

    Update functionn.c

--- functionn.c
+++ functionn.c
@@ -1,17 +1,18 @@
- #include<stdio.h>
+ //2.WAP to find greater number among two numbers using the return statement function.
+#include<stdio.h>
   //  FUNCTION DECLEARTION
   int greatestnum (int a,int b); 
   
   int main (){
   	
-  	int x,y,z;
-  	printf ("enter the num to check gareater:");
-  	scanf ("%d %d",&x,&y);
-     z= greatestnum(x , y);
-     printf("\n the greatest number is %d",z);
-     return 0;
+  	 int x,y,z;
+   	printf ("enter the num to check gareater:");
+   	scanf ("%d %d",&x,&y);
+    z= greatestnum(x , y);
+    printf("\n the greatest number is %d",z);
+    return 0;
   }
-  int greatestnum (int a, int b ){
+   int greatestnum (int a, int b ){
   	if (a > b)
   	return a;
   	else 
