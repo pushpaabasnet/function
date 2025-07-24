b7542dce0af11aa0d0d79a59186522d809666939
Pushpa Basnet <puspabasnet97@gmail.com>
2025-07-24 18:13:09 +0545

    Add files via upload

---
+++ functionn.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+  //  FUNCTION DECLEARTION
+  int greatestnum (int a,int b); 
+  
+  int main (){
+  	
+  	int x,y,z;
+  	printf ("enter the num to check gareater:");
+  	scanf ("%d %d",&x,&y);
+     z= greatestnum(x , y);
+     printf("\n the greatest number is %d",z);
+     return 0;
+  }
+  int greatestnum (int a, int b ){
+  	if (a > b)
+  	return a;
+  	else 
+  	return b;
+  }
